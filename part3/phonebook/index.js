b65e70fba498d6422437cdb0b47fb38466c2c19d
Jon Ziefle <jon.ziefle@gmail.com>
2021-12-22 00:25:28 -0500

    part3, exercises 3.7-3.8: phonebook

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+
+// morgan logging
+morgan.token('request-body', (req, res) => JSON.stringify(req.body))
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :request-body')
+)
 
 let persons = [
     { 
