1ea17a2aee3ce8c85edf78bcf2bf10fd47fe9acc
Jon Ziefle <jon.ziefle@gmail.com>
2021-12-26 22:45:37 -0500

    update notes app

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -1,9 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const cors = require('cors')
-
 const app = express()
+const Note = require('./models/note')
 
+const cors = require('cors')
 app.use(cors())
+
 app.use(express.json())
 app.use(express.static('build'))
 
@@ -53,39 +55,35 @@ app.get('/', (request, response) => {
 
 // get all notes
 app.get('/api/notes', (request, response) => {
-    response.json(notes)
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
 })
 
 // get note by id
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-    
-    if (note) {
+    Note.findById(request.params.id).then(note => {
         response.json(note)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 // add note
 app.post('/api/notes', (request, response) => {
     const body = request.body
   
-    if (!body.content) {
-        return response.status(400).json({ error: 'content missing' })
+    if (body.content === undefined) {
+      return response.status(400).json({ error: 'content missing' })
     }
   
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        date: new Date(),
-        id: generateId(),
-    }
+    const note = new Note({
+      content: body.content,
+      important: body.important || false,
+      date: new Date(),
+    })
   
-    notes = notes.concat(note)
-  
-    response.json(note)
+    note.save().then(savedNote => {
+      response.json(savedNote)
+    })
 })
 
 // delete note
