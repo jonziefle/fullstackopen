8a9eaa742357e030da31a23f8be6aeba62a6f4d3
Jon Ziefle <jon.ziefle@gmail.com>
2021-12-27 09:39:10 -0500

    notes app for heroku deploy

--- part3/notes/index.js
+++ part3/notes/index.js
@@ -35,78 +35,83 @@ app.get('/api/notes', (request, response) => {
 // get note by id
 app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id)
-      .then(note => {
-          if (note) {
+        .then(note => {
+            if (note) {
                 response.json(note)
             } else {
                 response.status(404).end()
             }
-      })
-      .catch(error => next(error))
+        })
+        .catch(error => next(error))
 })
 
 // add note
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
-  
+
     if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
+        return response.status(400).json({ error: 'content missing' })
     }
-  
+
     const note = new Note({
-      content: body.content,
-      important: body.important || false,
-      date: new Date(),
+        content: body.content,
+        important: body.important || false,
+        date: new Date(),
     })
-  
-    note.save().then(savedNote => {
-      response.json(savedNote)
-    })
+
+    note.save()
+        .then(savedNote => savedNote.toJSON())
+        .then(savedAndFormattedNote  => {
+            response.json(savedAndFormattedNote)
+        })
+        .catch(error => next(error))
 })
 
 // update note
 app.put('/api/notes/:id', (request, response, next) => {
     const body = request.body
-  
+
     const note = {
         content: body.content,
         important: body.important,
     }
-  
+
     Note.findByIdAndUpdate(request.params.id, note, { new: true })
-      .then(updatedNote => {
-          response.json(updatedNote)
-      })
-      .catch(error => next(error))
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
 })
 
 // delete note
 app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndRemove(request.params.id)
-      .then(result => {
-          response.status(204).end()
-      })
-      .catch(error => next(error))
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 // handle unknown endpoints
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
-  
+
 app.use(unknownEndpoint)
 
 // handle errors
 const errorHandler = (error, request, response, next) => {
     console.error(error.message)
-  
+
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
-  
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
     next(error)
-  }
-  
+}
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
