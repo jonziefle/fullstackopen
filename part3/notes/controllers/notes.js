30a34922d332586f034ae3367f6dae5bfec3f970
Jon Ziefle <jon.ziefle@gmail.com>
2022-01-03 23:07:27 -0500

    testing for notes app

--- part3/notes/controllers/notes.js
+++ part3/notes/controllers/notes.js
@@ -2,27 +2,23 @@ const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
 // get all notes
-notesRouter.get('/', (request, response) => {
-    Note.find({}).then(notes => {
-        response.json(notes)
-    })
+notesRouter.get('/', async (request, response) => {
+    const notes = await Note.find({})
+    response.json(notes)
 })
 
 // get note by id
-notesRouter.get('/:id', (request, response, next) => {
-    Note.findById(request.params.id)
-        .then(note => {
-            if (note) {
-                response.json(note)
-            } else {
-                response.status(404).end()
-            }
-        })
-        .catch(error => next(error))
+notesRouter.get('/:id', async (request, response) => {
+    const note = await Note.findById(request.params.id)
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
 })
 
 // add note
-notesRouter.post('/', (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
 
     const note = new Note({
@@ -31,11 +27,8 @@ notesRouter.post('/', (request, response, next) => {
         date: new Date(),
     })
 
-    note.save()
-        .then(savedNote => {
-            response.json(savedNote)
-        })
-        .catch(error => next(error))
+    const savedNote = await note.save()
+    response.json(savedNote)
 })
 
 // update note
@@ -55,12 +48,9 @@ notesRouter.put('/:id', (request, response, next) => {
 })
 
 // delete note
-notesRouter.delete('/:id', (request, response, next) => {
-    Note.findByIdAndRemove(request.params.id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response) => {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = notesRouter
