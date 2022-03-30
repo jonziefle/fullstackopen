3267a528934b2ceb115a8a9a3735b501b3fc4111
Jon Ziefle <jon.ziefle@gmail.com>
2022-03-29 22:15:08 -0400

    part6c, notes example

--- part6/notes/src/App.js
+++ part6/notes/src/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import NewNote from './components/NewNote'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notes from './components/Notes'
 
+import { initializeNotes } from './reducers/noteReducer'
+
 const App = () => {
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(initializeNotes())
+    }, [dispatch])
+
     return (
         <div>
             <NewNote />
